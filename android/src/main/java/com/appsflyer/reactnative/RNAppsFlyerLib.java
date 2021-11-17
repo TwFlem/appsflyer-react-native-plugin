59d4c8bace5a754d42b3c87f45b008c4c7d56441
Trenton Fleming <tfleming@totalwine.com>
2021-11-17 14:56:12 -0500

    expose the appsflyer lib instance

---
+++ android/src/main/java/com/appsflyer/reactnative/RNAppsFlyerLib.java
@@ -0,0 +1,9 @@
+package com.appsflyer.reactnative;
+
+import com.appsflyer.AppsFlyerLib;
+
+public class RNAppsFlyerLib {
+   public static AppsFlyerLib getInstance() {
+        return AppsFlyerLib.getInstance();
+   }
+}
