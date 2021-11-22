76f51097d36043ace8e8337dda8b31bcfe4d8616
Trenton Fleming <tfleming@totalwine.com>
2021-11-22 10:41:25 -0500

    expose a way to interact with AppsFlyerLib instance

--- android/src/main/java/com/appsflyer/reactnative/RNAppsFlyerLib.java
+++ android/src/main/java/com/appsflyer/reactnative/RNAppsFlyerLib.java
@@ -1,9 +1,15 @@
 package com.appsflyer.reactnative;
 
+import android.app.Activity;
+
 import com.appsflyer.AppsFlyerLib;
 
 public class RNAppsFlyerLib {
-   public static AppsFlyerLib getInstance() {
-        return AppsFlyerLib.getInstance();
-   }
+    public static void addPushNotificationDeepLinkPath(String ...path) {
+        AppsFlyerLib.getInstance().addPushNotificationDeepLinkPath(path);
+    }
+
+    public static void sendPushNotificationData(Activity activity) {
+        AppsFlyerLib.getInstance().sendPushNotificationData(activity);
+    }
 }
